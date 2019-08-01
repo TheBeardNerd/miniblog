0ea4a72a92dd94d4d8b176d73cfabead078a910d
Travis <travisdub1@gmail.com>
2019-07-31 22:18:55 -0500

    First Commit

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+        'post_id' => function () {
+            return factory('App\Post')->create()->id;
+        },
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'comment' => $faker->paragraph
+    ];
+});
