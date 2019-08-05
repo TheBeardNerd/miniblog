64d24f1908a7ce9abc6a44e58a3f95bffef428cb
Travis Windsor-Cummings <traviswc@elitedeals.com>
2019-08-05 14:50:40 -0500

    working on login and register routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,63 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title')</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600|Special+Elite&display=swap" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/6c02b20e8c.js"></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+        <!-- Custom Stylesheet -->
+        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
+
+        <!-- favicon -->
+        <link rel="icon" type="image/ico" href="{{ URL::asset('images/skull-crossbones.ico') }}">
+
+    </head>
+    <body>
+        <div class="border">
+            <nav class="navbar navbar-expand-lg nav-color fixed-top">
+                <a class="navbar-brand nav-logo" href="#">MiniBlog<span class="text-success ml-1"><i class="far fa-keyboard"></i></span></a>
+                <button class="navbar-toggler nav-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars fa-lg"></i>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
+                    <div class="navbar-nav links nav-menu mr-auto">
+                        <a class="nav-item nav-link" href="/">Home</a>
+                        {{-- <a class="nav-item nav-link" href="/contact">Contact</a></a>
+                        <a class="nav-item nav-link" href="/about">About</a></a> --}}
+                    </div>
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav links nav-menu ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <li class="nav-item links">
+                                <a class="nav-item nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt mr-2 text-success"></i>{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <li class="nav-item links">
+                                    <a class="nav-item nav-link" href="{{ route('register') }}"><i class="fas fa-user-plus mr-2 text-success"></i>{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <li class="nav-item links dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre><i class="fas fa-user-check"></i>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                <div class="dropdown-menu links text-dark dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item text-dark" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt text-success"></i>
                                         {{ __('Logout') }}
                                     </a>
 
@@ -68,13 +67,37 @@
                             </li>
                         @endguest
                     </ul>
+                    <div class="navbar-nav">
+                        <button type="button" class="btn btn-sm btn-success create">
+                            <a class="nav-item nav-link text-decoration-none text-white text-lg" href="/miniblogs/create">
+                                Create MiniBlog
+                            </a>
+                        </button>
+                    </div>
+                </div>
+            </nav>
+            <div class="content mar">
+                    @yield('content')
+            </div>
+        </div>
+        <footer class="navbar navbar-expand-lg nav-color py-5 mb-4">
+            <div class="container justify-content-center flex-column">
+                <div class="footer-links">
+                    <a href="https://www.linkedin.com/in/traviswindsorcummings"><i class="fab fa-linkedin-in fa-fw"></i></a>
+                    <a href="https://www.facebook.com/traviswindsorcummings"><i class="fab fa-facebook-f fa-fw"></i></a>
+                    <a href="https://www.instagram.com/traviswindsorcummings"><i class="fab fa-instagram fa-fw"></i></a>
+                    <a href="https://github.com/TheBeardNerd"><i class="fab fa-github fa-fw"></i></a>
+                    <a href="https://codepen.io/TravisWindsor-Cummings/"><i class="fab fa-codepen fa-fw"></i></a>
+                </div>
+                <div class="text-white pt-1">
+                    <p>Made with <i class="far fa-heart mx-1"></i> by Travis Windsor-Cummings</p>
                 </div>
             </div>
-        </nav>
+        </footer>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+        <!-- Bootstrap Jquery, Popper, and JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
 </html>
